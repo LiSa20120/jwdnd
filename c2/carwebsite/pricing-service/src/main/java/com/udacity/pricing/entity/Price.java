27223a7cef84381e3db9314a51ca6ed45161a785
LiSa20120 <lisaravi2017@gmail.com>
2022-11-16 00:25:54 +0530

    add eureka server

---
+++ c2/carwebsite/pricing-service/src/main/java/com/udacity/pricing/entity/Price.java
@@ -0,0 +1,19 @@
+package com.udacity.pricing.domain.price;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * Represents the price of a given vehicle, including currency.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Price {
+    private String currency;
+    private BigDecimal price;
+    private Long vehicleId;
+}
